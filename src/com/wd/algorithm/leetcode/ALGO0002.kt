e8fefca626af83ba2aa671997d0c59688462f59e
Walker Denial <WalkerDenial@qq.com>
2021-01-13 23:37:40 +0800

    update algorithm 2

--- src/com/wd/algorithm/leetcode/ALGO0002.kt
+++ src/com/wd/algorithm/leetcode/ALGO0002.kt
@@ -1,6 +1,8 @@
 package com.wd.algorithm.leetcode
 
-import org.w3c.dom.NodeList
+class ListNode(var `val`: Int) {
+    var next: ListNode? = null
+}
 
 /**
  * 2. 两数相加
@@ -14,17 +16,78 @@ class ALGO0002 {
 
     /**
      * 方式一
-     * 冒泡方式
-     * 时间复杂度 T(n²)
+     * 转换成对应的整数再进行计算，然后逆序输出
+     * 时间复杂度 T(n)
      */
-    fun addTwoNumbers(l1: NodeList, l2: NodeList): NodeList {
+    fun addTwoNumbers1(l1: ListNode?, l2: ListNode?): ListNode? {
+        val str1 = l1.contentToString() // l1 逆序
+        val str2 = l2.contentToString() // l2 逆序
+        val num1 = if (str1.isBlank()) 0 else str1.reversed().toInt() // l1 真实数值
+        val num2 = if (str2.isBlank()) 0 else str2.reversed().toInt() // l2 真是数值
+        val result = (num1 + num2).toString().reversed() // 求出结果并逆序
+        val data = IntArray(result.length)  // 转换成数组
+        for (i in result.indices) data[i] = result[i].toString().toInt()
+        return getListNode(data) // 获取返回结果
+    }
 
+    /**
+     * 方式二
+     * 按节点进位处理
+     * 时间复杂度 T(n)
+     */
+    fun addTwoNumbers2(l1: ListNode?, l2: ListNode?): ListNode? {
+        var node = ListNode(0) // 待返回数据
+        var curr:ListNode? = node // 待返回数据当前节点
+        var currNode1 = l1 // l1 当前节点
+        var currNode2 = l2 // l2 当前节点
+        var offset = 0 // 进位值
+        while (currNode1 != null || currNode2 != null) {
+            val result = (currNode1?.`val` ?: 0) + (currNode2?.`val` ?: 0) + offset
+            offset = result / 10
+            curr?.next = ListNode(result % 10)
+            curr = curr?.next
+            currNode1 = currNode1?.next
+            currNode2 = currNode2?.next
+        }
+        if(offset > 0) curr?.next = ListNode(offset) // 虽然测试结束，如果有进位，需要增加一个节点
+        return node.next
     }
 
 }
 
 fun main() {
-    val clazz = ALGO0001()
-    println(clazz.twoSum1(intArrayOf(42, 34, 1, 6, 7, 87, 45, 7, 4, 9, 2, 423, 126), 127).contentToString())
-    println(clazz.twoSum2(intArrayOf(42, 34, 1, 6, 7, 87, 45, 7, 4, 9, 2, 423, 126), 127).contentToString())
+    val clazz = ALGO0002()
+
+    val l1 = getListNode(intArrayOf(9, 9, 9, 9, 9, 9, 9))
+    val l2 = getListNode(intArrayOf(9, 9, 9, 9))
+
+    println("l1: " + l1.contentToString())
+    println("l2: " + l2.contentToString())
+
+    println("result: " + clazz.addTwoNumbers1(l1, l2).contentToString())
+    println("result: " + clazz.addTwoNumbers2(l1, l2).contentToString())
+}
+
+private fun getListNode(data: IntArray): ListNode? {
+    if (data.isEmpty()) return null
+    var lastNode: ListNode? = null
+    var node: ListNode? = null
+    for (i in data) {
+        val item = ListNode(i)
+        if (node == null) node = item
+        else lastNode?.next = item
+        lastNode = item
+    }
+    return node
+}
+
+private fun ListNode?.contentToString(): String {
+    if (this == null) return ""
+    val sb = StringBuilder()
+    var item: ListNode? = this
+    while (item != null) {
+        sb.append("${item.`val`}")
+        item = item.next
+    }
+    return sb.toString()
 }
