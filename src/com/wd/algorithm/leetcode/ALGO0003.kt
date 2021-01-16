25d8c4e026cb0df774b68161fdb60e8d36e33e08
Walker Denial <WalkerDenial@qq.com>
2021-01-16 11:24:32 +0800

    update algorithm 3

--- src/com/wd/algorithm/leetcode/ALGO0003.kt
+++ src/com/wd/algorithm/leetcode/ALGO0003.kt
@@ -1,5 +1,7 @@
 package com.wd.algorithm.leetcode
 
+import kotlin.math.max
+
 /**
  * 3. 无重复字符的最长子串
  *
@@ -11,15 +13,24 @@ class ALGO0003 {
     /**
      * 方式一：暴力解法
      * 逐个生产子字符串，看看是否含有重复的字符
-     * 时间复杂度 T(n³)
+     * 时间复杂度 T(n²)
      */
     fun lengthOfLongestSubstring1(s: String): Int {
-        if(s.isBlank()) return 0
-        var max = 0
+        if (s.isEmpty()) return 0
+        var endIndex = 0
+        var maxLength = 0
+        val set = hashSetOf<Char>()
         for (i in s.indices) {
-
+            set.clear()
+            set.add(s[i])
+            for (j in (i + 1) until s.length) {
+                endIndex = j
+                if (set.contains(s[j])) break
+                else set.add(s[j])
+            }
+            maxLength = max(maxLength, endIndex - i)
         }
-        return 0
+        return maxLength
     }
 
     /**
@@ -28,8 +39,18 @@ class ALGO0003 {
      * 时间复杂度 T(n)
      */
     fun lengthOfLongestSubstring2(s: String): Int {
-        if(s.isBlank()) return 0
-        return 0
+        if (s.isEmpty()) return 0
+        var startIndex = 0
+        var maxLength = 0
+        val map = mutableMapOf<Char, Int>()
+        for (i in s.indices) {
+            if (map.containsKey(s[i])) {
+                maxLength = max(maxLength, i - startIndex)
+                startIndex = map[s[i]]!! + 1
+            }
+            map[s[i]] = i
+        }
+        return maxLength
     }
 
 }
@@ -37,4 +58,10 @@ class ALGO0003 {
 fun main() {
     val clazz = ALGO0003()
     val str = "pwwkew"
+
+    println(clazz.lengthOfLongestSubstring1(" "))
+    println(clazz.lengthOfLongestSubstring2(" "))
+
+//    (clazz::lengthOfLongestSubstring1).test(str)
+//    (clazz::lengthOfLongestSubstring2).test(str)
 }
