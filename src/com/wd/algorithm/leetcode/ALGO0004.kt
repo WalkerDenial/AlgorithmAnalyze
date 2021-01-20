0a2c241a222e237cdfeca3204a97b8413d0174a0
Walker Denial <WalkerDenial@qq.com>
2021-01-20 22:53:42 +0800

    edit code

--- src/com/wd/algorithm/leetcode/ALGO0004.kt
+++ src/com/wd/algorithm/leetcode/ALGO0004.kt
@@ -13,7 +13,25 @@ class ALGO0004 {
      * 将 2 个数组合并，然后取中位数
      * 时间复杂度 T(n²)
      */
-    fun findMedianSortedArrays(nums1: IntArray, nums2: IntArray): Double {
+    fun findMedianSortedArrays1(nums1: IntArray, nums2: IntArray): Double {
+        val nums = nums1.plus(nums2)
+        nums.sort()
+        return when {
+            nums.isEmpty() -> 0.0
+            nums.size % 2 != 0 -> nums[nums.size / 2].toDouble()
+            else -> {
+                val index = nums.size / 2
+                (nums[index - 1] + nums[index]) / 2.0
+            }
+        }
+    }
+
+    /**
+     * 方式二：暴力解法
+     * 将 2 个数组合并，然后取中位数
+     * 时间复杂度 T(n²)
+     */
+    fun findMedianSortedArrays2(nums1: IntArray, nums2: IntArray): Double {
         return 0.0
     }
 
@@ -21,4 +39,8 @@ class ALGO0004 {
 
 fun main() {
     val clazz = ALGO0004()
+    val num1 = intArrayOf(1,2,3)
+    val num2 = intArrayOf(4,5,6)
+    println(clazz.findMedianSortedArrays1(num1, num2))
+    println(clazz.findMedianSortedArrays2(num1, num2))
 }
