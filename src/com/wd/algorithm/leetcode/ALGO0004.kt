51eed3ad5999099ad18cd7f0872674c95868e7de
Walker Denial <WalkerDenial@qq.com>
2021-01-21 22:59:41 +0800

    add algorithm4 code

--- src/com/wd/algorithm/leetcode/ALGO0004.kt
+++ src/com/wd/algorithm/leetcode/ALGO0004.kt
@@ -1,5 +1,7 @@
 package com.wd.algorithm.leetcode
 
+import com.wd.algorithm.test
+
 /**
  * 4. 寻找两个正序数组的中位数
  *
@@ -11,36 +13,53 @@ class ALGO0004 {
     /**
      * 方式一：暴力解法
      * 将 2 个数组合并，然后取中位数
-     * 时间复杂度 T(n²)
+     * 时间复杂度 T(m + n)
      */
     fun findMedianSortedArrays1(nums1: IntArray, nums2: IntArray): Double {
         val nums = nums1.plus(nums2)
         nums.sort()
+        val index = nums.size / 2
         return when {
-            nums.isEmpty() -> 0.0
-            nums.size % 2 != 0 -> nums[nums.size / 2].toDouble()
-            else -> {
-                val index = nums.size / 2
-                (nums[index - 1] + nums[index]) / 2.0
-            }
+            nums.size % 2 != 0 -> nums[index].toDouble()
+            else -> (nums[index - 1] + nums[index]) / 2.0
         }
     }
 
     /**
-     * 方式二：暴力解法
-     * 将 2 个数组合并，然后取中位数
-     * 时间复杂度 T(n²)
+     * 方式二：二分查找
+     * 通过二分查找找出分割位置，然后比较左上、右下 和 左下、右上数据信息，符合条件即为对接点
+     * 时间复杂度 T(min(m, n))
      */
     fun findMedianSortedArrays2(nums1: IntArray, nums2: IntArray): Double {
-        return 0.0
+        val totalNum = nums1.size + nums2.size
+        val isEven = totalNum % 2 == 0
+        val halfIndex = totalNum / 2
+        var index = 0
+        var left = 0
+        var right = 0
+        var middle = 0.0
+        while (left < nums1.size || right < nums2.size) {
+            val value = when {
+                left >= nums1.size -> nums2[right++]
+                right >= nums2.size -> nums1[left++]
+                nums1[left] > nums2[right] -> nums2[right++]
+                else -> nums1[left++]
+            }.toDouble()
+            if (index == halfIndex) {
+                middle = if (isEven) (middle + value) / 2 else value
+                break
+            } else middle = value
+            index++
+        }
+        return middle
     }
 
 }
 
 fun main() {
     val clazz = ALGO0004()
-    val num1 = intArrayOf(1,2,3)
-    val num2 = intArrayOf(4,5,6)
-    println(clazz.findMedianSortedArrays1(num1, num2))
-    println(clazz.findMedianSortedArrays2(num1, num2))
+    val num1 = intArrayOf(1, 2, 3)
+    val num2 = intArrayOf(4, 5, 6)
+    (clazz::findMedianSortedArrays1).test(num1, num2)
+    (clazz::findMedianSortedArrays2).test(num1, num2)
 }
